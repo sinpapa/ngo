a3687bf57cc2572b4713877637d7cb415d21ab13
guilty822 <guiltycrown822@gmail.com>
2016-06-06 22:24:08 +0900

    Test-Unity

---
+++ Test/Assets/Script/Cube.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+	float X_Speed = 1;
+	float Z_Speed = 1;
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float vertical = Input.GetAxis ("Vertical");
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		if (Input.GetKey ("up")) {
+			transform.Translate(0, 0, vertical * Z_Speed);
+		}
+		if (Input.GetKey ("down")) {
+			transform.Translate(0, 0, vertical * Z_Speed);
+		}
+		if (Input.GetKey("left")) {
+			transform.Translate(horizontal * X_Speed, 0, 0);
+		}
+		if (Input.GetKey("right")) {
+			transform.Translate(horizontal * X_Speed, 0, 0);
+		}
+
+	}
+
+
+}
